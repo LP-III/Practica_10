b578934d0897b7100a6bab9dd3aca6992ec8fbff
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2025-11-27 09:22:40 -0500

    Create Partida.java

---
+++ Partida.java
@@ -0,0 +1,12 @@
+public class Partida {
+    public int id;
+    public String nombre;
+    public String tiempo;
+    public String escenario;
+    public Partida(int id, String nombre, String tiempo, String escenario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+        this.escenario = escenario;
+    }
+}
