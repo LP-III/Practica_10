32d8a10d41569fcdc41a3b1d57d2d80405f84561
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2025-11-29 08:22:45 -0500

    Update SlotPartida.java

--- EjercicioJuego/SlotPartida.java
+++ EjercicioJuego/SlotPartida.java
@@ -4,22 +4,37 @@ public class SlotPartida extends JButton {
         setLayout(null);
         setBorderPainted(false);
         setContentAreaFilled(false);
+        setFocusPainted(false);
+
         setIcon(new ImageIcon("assets/slot_base.png"));
         setRolloverIcon(new ImageIcon("assets/slot_base_hover.png"));
         setSize(600, 140);
+        if (p != null) {
+            JLabel nombreImg = new JLabel(new ImageIcon("assets/slot_nombre.png"));
+            nombreImg.setBounds(40, 20, 400, 40);
+            add(nombreImg);
 
-        if(p != null){
-            JLabel nombre = new JLabel(new ImageIcon(TextoImagen.crearTexto("Jugador: " + p.nombre, 26)));
-            nombre.setBounds(40, 20, 400, 40);
-            add(nombre);
-            JLabel tiempo = new JLabel(new ImageIcon(TextoImagen.crearTexto("Tiempo: " + p.tiempo, 20)));
-            tiempo.setBounds(40, 65, 300, 40);
-            add(tiempo);
-            JLabel escenario = new JLabel(new ImageIcon(TextoImagen.crearTexto("Escenario: " + p.escenario, 20)));
-            escenario.setBounds(260, 65, 300, 40);
-            add(escenario);
-        } else {
-            JLabel vacio = new JLabel(new ImageIcon(TextoImagen.crearTexto("Vacío", 28)));
+            JLabel tiempoImg = new JLabel(new ImageIcon("assets/slot_tiempo.png"));
+            tiempoImg.setBounds(40, 65, 300, 40);
+            add(tiempoImg);
+
+            JLabel escenarioImg = new JLabel(new ImageIcon("assets/slot_escenario.png"));
+            escenarioImg.setBounds(260, 65, 300, 40);
+            add(escenarioImg);
+
+            JLabel txtNombre = new JLabel(p.getNombre());
+            txtNombre.setBounds(140, 20, 300, 40);
+            add(txtNombre);
+
+            JLabel txtTiempo = new JLabel(p.getTiempo());
+            txtTiempo.setBounds(140, 65, 150, 40);
+            add(txtTiempo);
+
+            JLabel txtEscenario = new JLabel(p.getEscenario());
+            txtEscenario.setBounds(370, 65, 150, 40);
+            add(txtEscenario);
+        }else{
+            JLabel vacio = new JLabel(new ImageIcon("assets/slot_vacio.png"));
             vacio.setBounds(230, 45, 200, 40);
             add(vacio);
         }
