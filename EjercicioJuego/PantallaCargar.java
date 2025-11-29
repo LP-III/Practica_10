4ac7b2d6d2107b8914a6eab2e82ac475b4909765
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2025-11-29 08:22:27 -0500

    Update PantallaCargar.java

--- EjercicioJuego/PantallaCargar.java
+++ EjercicioJuego/PantallaCargar.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 public class PantallaCargar extends JPanel {
 
     private JFrame ventana;
@@ -9,71 +11,81 @@ public class PantallaCargar extends JPanel {
     public PantallaCargar(JFrame ventana) {
         this.ventana = ventana;
         setLayout(null);
-        db = new DBManager();
+
         fondo = new ImageIcon("assets/fondo_juego.png").getImage();
+        db = new DBManager();
+
         colocarTitulo();
         colocarSlots();
-        colocarBotonRegresar();
+        colocarBotonVolver();
     }
 
     private void colocarTitulo() {
         JLabel titulo = new JLabel(new ImageIcon("assets/titulo_cargar.png"));
-        titulo.setBounds(260, 40, 500, 120);
+        titulo.setBounds(250, 30, 500, 100);
         add(titulo);
     }
 
     private void colocarSlots() {
-
         Partida p1 = db.getPartida(1);
         Partida p2 = db.getPartida(2);
         Partida p3 = db.getPartida(3);
 
-        SlotPartida s1 = new SlotPartida(p1);
-        SlotPartida s2 = new SlotPartida(p2);
-        SlotPartida s3 = new SlotPartida(p3);
-        s1.setBounds(250, 200, 600, 140);
-        s2.setBounds(250, 360, 600, 140);
-        s3.setBounds(250, 520, 600, 140);
-        s1.addActionListener(new CargarAccion(1));
-        s2.addActionListener(new CargarAccion(2));
-        s3.addActionListener(new CargarAccion(3));
-        add(s1);
-        add(s2);
-        add(s3);
+        SlotPartida slot1 = new SlotPartida(p1);
+        slot1.setBounds(200, 180, 600, 140);
+        add(slot1);
+        SlotPartida slot2 = new SlotPartida(p2);
+        slot2.setBounds(200, 340, 600, 140);
+        add(slot2);
+        SlotPartida slot3 = new SlotPartida(p3);
+        slot3.setBounds(200, 500, 600, 140);
+        add(slot3);
+
+        slot1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cargarPartida(1);
+            }
+        });
+        slot2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cargarPartida(2);
+            }
+        });
+        slot3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cargarPartida(3);
+            }
+        });
     }
 
-    private class CargarAccion implements java.awt.event.ActionListener {
-        private int id;
-
-        public CargarAccion(int id) {
-            this.id = id;
+    private void cargarPartida(int idSlot) {
+        Partida p = db.getPartida(idSlot);
+        if (p == null) {
+            JOptionPane.showMessageDialog(null, "No hay partida guardada");
+            return;
         }
-
-        public void actionPerformed(java.awt.event.ActionEvent e) {
-            System.out.println("Cargar partida: " + id);
-            ventana.setContentPane(new PantallaPrincipal(ventana));
-            ventana.revalidate();
-        }
+        ventana.setContentPane(new PantallaPrincipal(ventana, idSlot));
+        ventana.revalidate();
     }
 
-    private void colocarBotonRegresar() {
-        JButton b = new JButton(new ImageIcon("assets/boton_regresar.png"));
-        b.setRolloverIcon(new ImageIcon("assets/boton_regresar_hover.png"));
+    private void colocarBotonVolver() {
+        JButton volver = new JButton(new ImageIcon("assets/boton_regresar.png"));
+        volver.setRolloverIcon(new ImageIcon("assets/boton_regresar_hover.png"));
+        volver.setBorderPainted(false);
+        volver.setContentAreaFilled(false);
+        volver.setFocusPainted(false);
+        volver.setBounds(20, 20, 150, 60);
 
-        b.setBorderPainted(false);
-        b.setContentAreaFilled(false);
-        b.setBounds(20, 20, 150, 60);
-        b.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent e) {
+        volver.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 ventana.setContentPane(new PantallaInicio(ventana));
                 ventana.revalidate();
             }
         });
-
-        add(b);
+        add(volver);
     }
 
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
     }
